ea676b99486cfdafe6341e08163aff5fdaaa6d92
TrongPhucNguyen <trongnguyenphuc22082003@gmail.com>
2024-09-10 07:56:46 +0700

    add .

---
+++ src/config/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connect successfully!!!');
+    } catch (error) {
+        console.log('Connect failure!!!');
+    }
+}
+
+module.exports = { connect };
---
+++ src/model/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    gender: { type: String, enum: ["MALE", "FEMALE", "OTHER"] },
+    isAdmin: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true },
+    address: { type: String },
+    otp: { type: String },
+    is_verified: { type: Boolean, default: false },
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+
