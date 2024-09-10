ecb61b8989b475c20888be9a5236dd8d2a9217b6
TrongPhucNguyen <trongnguyenphuc22082003@gmail.com>
2024-09-10 09:01:17 +0700

    update db connect

--- src/config/db/dbconnection.js
+++ src/config/db/dbconnection.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://admin:admin123456@web-ecommerce.b0gyw.mongodb.net/?retryWrites=true&w=majority&appName=web-ecommerce");
+        await mongoose.connect("mongodb+srv://admin:admin123456@web-ecommerce.b0gyw.mongodb.net/web-ecommerce-shop?retryWrites=true&w=majority&appName=web-ecommerce", {
+            autoIndex: true,
+        });
         console.log("Database connected successfully");
     } catch (e) {
         console.error(e);
